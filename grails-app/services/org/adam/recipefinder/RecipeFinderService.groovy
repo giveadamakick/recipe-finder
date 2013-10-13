fe6a0782de5f9c26e457c8404d05b71f76809146
Adam Hynes <hynes.adam@gmail.com>
2013-10-13 11:46:40 +1100

    Fixed bug in recipe finder service (unable to use object as map key - string now used. Added specs

--- grails-app/services/org/adam/recipefinder/RecipeFinderService.groovy
+++ grails-app/services/org/adam/recipefinder/RecipeFinderService.groovy
@@ -3,81 +3,81 @@ package org.adam.recipefinder
 class RecipeFinderService {
 
     def findRecipe(itemsInFridge, recipesAvailable) {
-    	def useableItemsMap = filterItemsPastUseByDate(itemsInFridge)
-    	def closestUseByToRecipeMap = findCookableRecipes(recipesAvailable, useableItemsMap)
+        def useableItemsMap = filterItemsPastUseByDate(itemsInFridge)
+        def closestUseByToRecipeMap = findCookableRecipes(recipesAvailable, useableItemsMap)
 
-    	def result
-    	if(closestUseByToRecipeMap.isEmpty())
-    	{
-    		result = null
-    	}
-    	else
-    	{
-    		result = closestUseByToRecipeMap.sort().entrySet().toList().first().value
-    	}
+        def result
+        if(closestUseByToRecipeMap.isEmpty())
+        {
+            result = null
+        }
+        else
+        {
+            result = closestUseByToRecipeMap.sort().entrySet().toList().first().value
+        }
 
-    	return result
+        return result
     }
 
     private def filterItemsPastUseByDate(fridgeItems)
     {
-    	def useableItemsMap = [:]
-    	def now = new Date().clearTime()
-    	for(FridgeItem fridgeItem : fridgeItems)
-    	{
-    		if(fridgeItem.useBy > now)
-    		{
-    			useableItemsMap.put(fridgeItem.item, [fridgeItem.amount, fridgeItem.useBy])
-    		}
-    	}
+        def useableItemsMap = [:]
+        def now = new Date().clearTime()
+        for(FridgeItem fridgeItem : fridgeItems)
+        {
+            if(fridgeItem.useBy > now)
+            {
+                useableItemsMap.put(fridgeItem.item.name, [fridgeItem.amount, fridgeItem.useBy])
+            }
+        }
 
-    	return useableItemsMap
+        return useableItemsMap
     }
 
     private def findCookableRecipes(recipesAvailable, Map useableItemsMap)
     {
-    	def closestUseByToRecipeMap = [:]
+        def closestUseByToRecipeMap = [:]
 
-    	for(Recipe recipe : recipesAvailable)
-    	{
-    		def allIngredientsAvailable = true
-    		def closestUseByDate = null
-    		for(Ingredient ingredient : recipe.ingredients)
-    		{
-    			if(useableItemsMap.containsKey(ingredient.item))
-    			{
-    				def amountInFridge = useableItemsMap.get(ingredient.item)[0]
-    				if(amountInFridge < ingredient.amount)
-    				{
-    					allIngredientsAvailable = false;
-    					break;
-    				}
-    				else
-    				{
-    					def itemUseByDate = useableItemsMap.get(ingredient.item)[1]
-    					if(closestUseByDate == null)
-    					{
-    						closestUseByDate = itemUseByDate
-    					}
-    					else
-    					{
-    						closestUseByDate = (itemUseByDate < closestUseByDate) ? itemUseByDate : closestUseByDate
-    					}
-    				}
-    			}
-    			else
-    			{
-    				allIngredientsAvailable = false;
-    				break;
-    			}
-    		}
+        for(Recipe recipe : recipesAvailable)
+        {
+            def allIngredientsAvailable = true
+            def closestUseByDate = null
+            for(Ingredient ingredient : recipe.ingredients)
+            {
+                if(useableItemsMap.containsKey(ingredient.item.name))
+                {
+                    def amountInFridge = useableItemsMap.get(ingredient.item.name)[0]
+                    if(amountInFridge < ingredient.amount)
+                    {
+                        allIngredientsAvailable = false;
+                        break;
+                    }
+                    else
+                    {
+                        def itemUseByDate = useableItemsMap.get(ingredient.item.name)[1]
+                        if(closestUseByDate == null)
+                        {
+                            closestUseByDate = itemUseByDate
+                        }
+                        else
+                        {
+                            closestUseByDate = (itemUseByDate < closestUseByDate) ? itemUseByDate : closestUseByDate
+                        }
+                    }
+                }
+                else
+                {
+                    allIngredientsAvailable = false;
+                    break;
+                }
+            }
 
-    		if(allIngredientsAvailable)
-    		{
-    			closestUseByToRecipeMap.put(closestUseByDate, recipe)
-    		}
-    	}
+            if(allIngredientsAvailable)
+            {
+                closestUseByToRecipeMap.put(closestUseByDate, recipe)
+            }
+        }
 
-    	return closestUseByToRecipeMap
+        return closestUseByToRecipeMap
     }
 }
--- test/unit/org/adam/recipefinder/RecipeFinderServiceSpec.groovy
+++ test/unit/org/adam/recipefinder/RecipeFinderServiceSpec.groovy
@@ -9,12 +9,173 @@ import spock.lang.Specification
 @TestFor(RecipeFinderService)
 class RecipeFinderServiceSpec extends Specification {
 
-    def setup() {
+    void "test that null is returned with no fridge items"() {
+        
+        setup:
+        def fridgeItems = []
+        def recipes = []
+
+        when:
+        def grilledCheeseIngredients = []
+        grilledCheeseIngredients.add(new Ingredient(amount:2, item:new Item(name:'bread', unit:Unit.SLICES)))
+        grilledCheeseIngredients.add(new Ingredient(amount:2, item:new Item(name:'cheese', unit:Unit.SLICES)))
+        recipes.add(new Recipe(name:'grilled cheese on toast', ingredients:grilledCheeseIngredients))
+        def saladSandwichIngredients = []
+        saladSandwichIngredients.add(new Ingredient(amount:2, item:new Item(name:'bread', unit:Unit.SLICES)))
+        saladSandwichIngredients.add(new Ingredient(amount:100, item:new Item(name:'mixed salad', unit:Unit.GRAMS)))
+        recipes.add(new Recipe(name:'salad sandwich', ingredients:saladSandwichIngredients))
+
+        def result = service.findRecipe(fridgeItems, recipes)
+
+        then:
+        result == null
     }
 
-    def cleanup() {
+    void "test that null is returned with no recipes"() {
+        
+        setup:
+        def fridgeItems = []
+        def recipes = []
+
+        when:
+        fridgeItems.add(new FridgeItem(item:new Item(name:'bread', unit:Unit.SLICES),amount:10,useBy:new Date().parse("d/M/yyyy", "25/12/2014")))
+        fridgeItems.add(new FridgeItem(item:new Item(name:'cheese', unit:Unit.SLICES),amount:10,useBy:new Date().parse("d/M/yyyy", "25/12/2014")))
+        fridgeItems.add(new FridgeItem(item:new Item(name:'butter', unit:Unit.GRAMS),amount:250,useBy:new Date().parse("d/M/yyyy", "25/12/2014")))
+        fridgeItems.add(new FridgeItem(item:new Item(name:'peanut butter', unit:Unit.GRAMS),amount:250,useBy:new Date().parse("d/M/yyyy", "2/12/2014")))
+        fridgeItems.add(new FridgeItem(item:new Item(name:'mixed salad', unit:Unit.GRAMS),amount:150,useBy:new Date().parse("d/M/yyyy", "26/12/2013")))
+
+        def result = service.findRecipe(fridgeItems, recipes)
+
+        then:
+        result == null
     }
 
-    void "test something"() {
+    void "test that null is returned with no fridge items or recipes"() {
+        
+        setup:
+        def fridgeItems = []
+        def recipes = []
+
+        when:
+        def result = service.findRecipe(fridgeItems, recipes)
+
+        then:
+        result == null
+    }
+
+    void "test that null is returned with no candidate recipes"() {
+        
+        setup:
+        def fridgeItems = []
+        def recipes = []
+
+        when:
+        fridgeItems.add(new FridgeItem(item:new Item(name:'cheese', unit:Unit.SLICES),amount:10,useBy:new Date().parse("d/M/yyyy", "25/12/2014")))
+        fridgeItems.add(new FridgeItem(item:new Item(name:'butter', unit:Unit.GRAMS),amount:250,useBy:new Date().parse("d/M/yyyy", "25/12/2014")))
+        fridgeItems.add(new FridgeItem(item:new Item(name:'peanut butter', unit:Unit.GRAMS),amount:250,useBy:new Date().parse("d/M/yyyy", "2/12/2014")))
+
+        def grilledCheeseIngredients = []
+        grilledCheeseIngredients.add(new Ingredient(amount:2, item:new Item(name:'bread', unit:Unit.SLICES)))
+        grilledCheeseIngredients.add(new Ingredient(amount:2, item:new Item(name:'cheese', unit:Unit.SLICES)))
+        recipes.add(new Recipe(name:'grilled cheese on toast', ingredients:grilledCheeseIngredients))
+        def saladSandwichIngredients = []
+        saladSandwichIngredients.add(new Ingredient(amount:2, item:new Item(name:'bread', unit:Unit.SLICES)))
+        saladSandwichIngredients.add(new Ingredient(amount:100, item:new Item(name:'mixed salad', unit:Unit.GRAMS)))
+        recipes.add(new Recipe(name:'salad sandwich', ingredients:saladSandwichIngredients))
+
+        def result = service.findRecipe(fridgeItems, recipes)
+
+        then:
+        result == null
+    }
+
+
+    void "test that recipe with gone-off ingredient is not chosen"() {
+        
+        setup:
+        def fridgeItems = []
+        def recipes = []
+
+        when:
+        fridgeItems.add(new FridgeItem(item:new Item(name:'bread', unit:Unit.SLICES),amount:10,useBy:new Date().parse("d/M/yyyy", "25/12/2014")))
+        fridgeItems.add(new FridgeItem(item:new Item(name:'cheese', unit:Unit.SLICES),amount:10,useBy:new Date().parse("d/M/yyyy", "25/12/2014")))
+        fridgeItems.add(new FridgeItem(item:new Item(name:'butter', unit:Unit.GRAMS),amount:250,useBy:new Date().parse("d/M/yyyy", "25/12/2014")))
+        fridgeItems.add(new FridgeItem(item:new Item(name:'peanut butter', unit:Unit.GRAMS),amount:250,useBy:new Date().parse("d/M/yyyy", "2/12/2014")))
+        fridgeItems.add(new FridgeItem(item:new Item(name:'milk', unit:Unit.ML),amount:500,useBy:new Date().parse("d/M/yyyy", "08/09/2013")))
+        fridgeItems.add(new FridgeItem(item:new Item(name:'eggs', unit:Unit.OF),amount:6,useBy:new Date().parse("d/M/yyyy", "26/09/2013")))
+        fridgeItems.add(new FridgeItem(item:new Item(name:'vegemite', unit:Unit.GRAMS),amount:100,useBy:new Date().parse("d/M/yyyy", "25/12/2014")))
+
+        def vegemiteOnToastIngredients = []
+        vegemiteOnToastIngredients.add(new Ingredient(amount:2, item:new Item(name:'bread', unit:Unit.SLICES)))
+        vegemiteOnToastIngredients.add(new Ingredient(amount:20, item:new Item(name:'butter', unit:Unit.GRAMS)))
+        vegemiteOnToastIngredients.add(new Ingredient(amount:25, item:new Item(name:'vegemite', unit:Unit.GRAMS)))
+        recipes.add(new Recipe(name:'vegemite on toast', ingredients:vegemiteOnToastIngredients))
+        def scrambledEggsIngredients = []
+        scrambledEggsIngredients.add(new Ingredient(amount:4, item:new Item(name:'eggs', unit:Unit.OF)))
+        scrambledEggsIngredients.add(new Ingredient(amount:400, item:new Item(name:'milk', unit:Unit.ML)))
+        recipes.add(new Recipe(name:'scrambled eggs', ingredients:scrambledEggsIngredients))
+
+        def result = service.findRecipe(fridgeItems, recipes)
+
+        then:
+        result != null
+        result.name == 'vegemite on toast'
+    }    
+
+    void "test that correct recipe is chosen with only one candidate recipe"() {
+        
+        setup:
+        def fridgeItems = []
+        def recipes = []
+
+        when:
+        fridgeItems.add(new FridgeItem(item:new Item(name:'bread', unit:Unit.SLICES),amount:10,useBy:new Date().parse("d/M/yyyy", "25/12/2014")))
+        fridgeItems.add(new FridgeItem(item:new Item(name:'cheese', unit:Unit.SLICES),amount:10,useBy:new Date().parse("d/M/yyyy", "25/12/2014")))
+        fridgeItems.add(new FridgeItem(item:new Item(name:'butter', unit:Unit.GRAMS),amount:250,useBy:new Date().parse("d/M/yyyy", "25/12/2014")))
+        fridgeItems.add(new FridgeItem(item:new Item(name:'peanut butter', unit:Unit.GRAMS),amount:250,useBy:new Date().parse("d/M/yyyy", "2/12/2014")))
+
+        def grilledCheeseIngredients = []
+        grilledCheeseIngredients.add(new Ingredient(amount:2, item:new Item(name:'bread', unit:Unit.SLICES)))
+        grilledCheeseIngredients.add(new Ingredient(amount:2, item:new Item(name:'cheese', unit:Unit.SLICES)))
+        recipes.add(new Recipe(name:'grilled cheese on toast', ingredients:grilledCheeseIngredients))
+        def saladSandwichIngredients = []
+        saladSandwichIngredients.add(new Ingredient(amount:2, item:new Item(name:'bread', unit:Unit.SLICES)))
+        saladSandwichIngredients.add(new Ingredient(amount:100, item:new Item(name:'mixed salad', unit:Unit.GRAMS)))
+        recipes.add(new Recipe(name:'salad sandwich', ingredients:saladSandwichIngredients))
+
+        def result = service.findRecipe(fridgeItems, recipes)
+
+        then:
+        result != null
+        result.name == 'grilled cheese on toast'
+    }
+
+    void "test that recipe with closest use by date is chosen when there are two candidate recipes"() {
+        
+        setup:
+        def fridgeItems = []
+        def recipes = []
+
+        when:
+        fridgeItems.add(new FridgeItem(item:new Item(name:'bread', unit:Unit.SLICES),amount:10,useBy:new Date().parse("d/M/yyyy", "25/12/2014")))
+        fridgeItems.add(new FridgeItem(item:new Item(name:'cheese', unit:Unit.SLICES),amount:10,useBy:new Date().parse("d/M/yyyy", "25/12/2014")))
+        fridgeItems.add(new FridgeItem(item:new Item(name:'butter', unit:Unit.GRAMS),amount:250,useBy:new Date().parse("d/M/yyyy", "25/12/2014")))
+        fridgeItems.add(new FridgeItem(item:new Item(name:'peanut butter', unit:Unit.GRAMS),amount:250,useBy:new Date().parse("d/M/yyyy", "2/12/2014")))
+        fridgeItems.add(new FridgeItem(item:new Item(name:'mixed salad', unit:Unit.GRAMS),amount:150,useBy:new Date().parse("d/M/yyyy", "26/12/2013")))
+
+        def grilledCheeseIngredients = []
+        grilledCheeseIngredients.add(new Ingredient(amount:2, item:new Item(name:'bread', unit:Unit.SLICES)))
+        grilledCheeseIngredients.add(new Ingredient(amount:2, item:new Item(name:'cheese', unit:Unit.SLICES)))
+        recipes.add(new Recipe(name:'grilled cheese on toast', ingredients:grilledCheeseIngredients))
+        def saladSandwichIngredients = []
+        saladSandwichIngredients.add(new Ingredient(amount:2, item:new Item(name:'bread', unit:Unit.SLICES)))
+        saladSandwichIngredients.add(new Ingredient(amount:100, item:new Item(name:'mixed salad', unit:Unit.GRAMS)))
+        recipes.add(new Recipe(name:'salad sandwich', ingredients:saladSandwichIngredients))
+
+        def result = service.findRecipe(fridgeItems, recipes)
+
+        then:
+        result != null
+        result.name == 'salad sandwich'
     }
 }
